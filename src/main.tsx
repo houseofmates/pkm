dc0617d3b253b473cd22c36cd1d5bc82dd8e6527
House <house@example.com>
2025-12-23 19:20:04 -0800

    Auto-save: 2025-12-23 19:20:04

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 import React from 'react';
 
 class ErrorBoundary extends React.Component<{ children: React.ReactNode }, { hasError: boolean, error: Error | null }> {
