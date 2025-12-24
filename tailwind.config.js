6d4f29ca3815a88874724d73abc4d3175e1fcf1c
House <house@example.com>
2025-12-23 19:02:45 -0800

    Auto-save: 2025-12-23 19:02:45

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,6 +60,9 @@ export default {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
+            fontFamily: {
+                varela: ['"Varela Round"', 'sans-serif'],
+            },
         },
     },
     plugins: [require("tailwindcss-animate")],
