d3837aa35020d65a7f7c67dcbe2edf6ac319c3d3
House <house@example.com>
2025-12-26 16:49:16 -0800

    auto-sync: 2025-12-26 16:49:16

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/house/pkm
 
-# 1. add everything
+# add changes
 git add .
 
-# 2. commit with current timestamp
-git commit -m "pkm sync: $(date +'%Y-%m-%d %H:%M:%S')"
-
-# 3. push to github. 
-# if it fails due to history issues, we force it because local is the "brain"
-git push origin main --force
+# commit only if there are changes (-q for quiet)
+if ! git diff-index --quiet HEAD; then
+    git commit -m "auto-sync: $(date +'%Y-%m-%d %H:%M:%S')" -q
+    # push only if commit was successful
+    git push origin main --force -q
+fi
