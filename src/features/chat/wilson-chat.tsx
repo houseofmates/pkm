0d81e619b46f4fdbb6bf8fa161b10a74c8890876
House <house@example.com>
2026-02-21 19:51:35 -0800

    auto-sync: local changes 2026-02-22T03:51:34.936Z

--- src/features/chat/wilson-chat.tsx
+++ src/features/chat/wilson-chat.tsx
@@ -54,7 +54,7 @@ export function WilsonChat() {
   }
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === 'enter') {
+    if (e.key === 'Enter') {
       checkAndSend()
     }
   }
