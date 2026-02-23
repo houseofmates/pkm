435628f4a9d15db87b4d94494f4d5149254b7079
House <house@example.com>
2026-02-22 16:16:49 -0800

    auto-sync: local changes 2026-02-23T00:16:49.269Z

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,7 +8,8 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
+  // Transform ESM packages in node_modules (like react-leaflet)
   transformIgnorePatterns: [
-    '/node_modules/'
+    '/node_modules/(?!(react-leaflet|@react-leaflet|leaflet)/)'
   ]
 };
