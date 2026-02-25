bc8805c004f56833adef93c0502d15387e999c33
House <house@example.com>
2026-02-24 18:09:36 -0800

    auto-sync: local changes 2026-02-25T02:09:36.075Z

--- src/features/records/components/record-context-menu.tsx
+++ src/features/records/components/record-context-menu.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 import { useAuth } from '@/contexts/auth-context';
 import { useAppSetting } from '@/hooks/use-app-setting';
 import {
