e2d7f27572da9e91f7cbc0acc283107e9c6316ac
House <house@example.com>
2026-02-25 14:49:31 -0800

    auto-sync: local changes 2026-02-25T22:49:31.267Z

--- src/features/page/components/PageCanvas.tsx
+++ src/features/page/components/PageCanvas.tsx
@@ -232,7 +232,7 @@ export function PageCanvas() {
         <div className="mx-auto" style={{ maxWidth: gridWidth }}>
           <GridLayout
             className="layout"
-            colNum={4}
+            cols={4}
             rowHeight={120}
             width={gridWidth}
             margin={[16, 16] as [number, number]}
