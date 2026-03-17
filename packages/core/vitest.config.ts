9a7710b43fc62a7372836cd1b867c260de6961cd
House <house@example.com>
2026-03-17 13:58:37 -0700

    auto-sync: local changes 2026-03-17T20:58:37.231Z

---
+++ packages/core/vitest.config.ts
@@ -0,0 +1,21 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: path.resolve(__dirname, '../../tests/setup-test.ts'),
+    exclude: ['**/node_modules/**', '**/e2e/**', '**/dist/**'],
+    include: [
+      'tests/**/*.{test,spec}.{ts,tsx}',
+      'src/**/*.{test,spec}.{ts,tsx}',
+      'src/features/**/__tests__/*.{test,spec}.{ts,tsx}'
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
