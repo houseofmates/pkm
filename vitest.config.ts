358621194e20d0e763f514f1d49d8a7aeeed6a07
House <house@example.com>
2026-03-19 22:11:40 -0700

    auto-sync: local changes 2026-03-20T05:11:40.687Z

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
+    threads: false,
     setupFiles: './tests/setup-test.ts',
     exclude: ['**/node_modules/**', '**/e2e/**', '**/dist/**'],
     include: [
