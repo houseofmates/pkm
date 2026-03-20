2326d72f66f8eb44f5cf95646b787286c0452834
House <house@example.com>
2026-03-19 22:49:02 -0700

    auto-sync: local changes 2026-03-20T05:49:02.705Z

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,7 +17,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': [
+        path.resolve(__dirname, './src'),
+        path.resolve(__dirname, './packages/core/src')
+      ]
     }
   }
 });
