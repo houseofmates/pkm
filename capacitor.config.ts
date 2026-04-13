dda70ad5fff6c0d3a227acbdfa7de7c592cf1fad
House <house@example.com>
2026-04-12 17:53:40 -0700

    auto-sync: local changes 2026-04-13T00:53:40.142Z

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.houseofmates.pkm',
   appName: 'pkm',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    haptics: {
+      enabled: true
+    },
+    pushnotifications: {
+      presentationOptions: ['badge', 'sound', 'alert']
+    }
+  }
 };
 
 export default config;
