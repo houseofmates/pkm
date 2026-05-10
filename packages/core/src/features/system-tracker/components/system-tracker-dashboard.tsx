7e50144dbd01656d80191df0d4e443c6871a0c0b
House <house@example.com>
2026-05-10 01:22:13 -0700

    auto-sync: local changes 2026-05-10T08:22:12.987Z

--- packages/core/src/features/system-tracker/components/system-tracker-dashboard.tsx
+++ packages/core/src/features/system-tracker/components/system-tracker-dashboard.tsx
@@ -26,6 +26,7 @@ import { MemberForm } from './members/member-form';
 import { FrontPanel } from './front/front-panel';
 import { FrontTimeline } from './front/front-timeline';
 import { GroupsView } from './groups/groups-view';
+import { JournalView } from './journal/journal-view';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import type { SystemMember } from '../types/schema';
 
